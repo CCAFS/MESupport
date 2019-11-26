c6789893c720c6a79374267119a73823f2a6bf6c
Sebastian Amariles <sebas932@gmail.com>
2019-11-26 18:03:22 -0500

    test solution #2

--- src/routes/api/guidelines.php
+++ src/routes/api/guidelines.php
@@ -13,8 +13,8 @@ $app->add(function ($req, $res, $next) {
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            //->withHeader('Content-Type', 'application/json');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withHeader('Content-Type', 'application/json');
 });
 
 
